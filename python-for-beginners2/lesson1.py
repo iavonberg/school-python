138aad505d7cadfdab3e8a948bee816198a33ccb
Iris Vonberg <irisavonberg@gmail.com>
2023-04-29 02:18:27 -0700

    second course start

---
+++ python-for-beginners2/lesson1.py
@@ -0,0 +1,13 @@
+exampleList = [1,6,7,3,6,9,0]
+
+for thing in exampleList:
+    print(thing)
+
+for x in range (1,11):
+    print(x)
+
+condition = 1
+while condition < 10:
+    print(condition)
+    condition += 1
+
